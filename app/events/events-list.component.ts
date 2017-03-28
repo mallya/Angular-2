6644ccb36e3ff91d20580596f4f66a0cd9d001ed
Sunil Mallya <ssmallya@gmail.com>
2017-03-28 14:27:11 -0500

    first 2 chapters.

---
+++ app/events/events-list.component.ts
@@ -0,0 +1,28 @@
+import {Component} from '@angular/core'
+@Component({
+    selector: 'events-list',
+    template: `
+<div>
+<h1>Upcoming Angular 2 events </h1>
+<event-thumbnail
+[event]="event1"></event-thumbnail>
+
+</div>
+`
+})
+export class EventsListComponent {
+    event1 = {
+        id: 1,
+        name: 'Angular Connect',
+        date: '9/26/2036',
+        time: '10:00 AM',
+        price: 599.99,
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        location: {
+            address: '1057 DT',
+            city: 'London',
+            country: 'England'
+        }
+    }
+    
+}
