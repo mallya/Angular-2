a17e26cae40749eaa61eb14a8193e5a5a91f3433
Sunil Mallya <ssmallya@gmail.com>
2017-03-28 23:06:57 -0500

    styles and expressions

--- app/events/event-thumbnail.component.js
+++ app/events/event-thumbnail.component.js
@@ -12,8 +12,9 @@ var core_1 = require('@angular/core');
 var EventThumbnailComponent = (function () {
     function EventThumbnailComponent() {
     }
-    EventThumbnailComponent.prototype.logFoo = function () {
-        console.log("foo");
+    EventThumbnailComponent.prototype.getStartTimeClass = function () {
+        var isEarlyStart = this.event && this.event.time === '8:00 am';
+        return { green: isEarlyStart, bold: isEarlyStart };
     };
     __decorate([
         core_1.Input(), 
@@ -22,8 +23,8 @@ var EventThumbnailComponent = (function () {
     EventThumbnailComponent = __decorate([
         core_1.Component({
             selector: 'event-thumbnail',
-            template: "\n<div class=\"well hoverwell thumbnail\">\n<h2>{{event.name}}</h2>\n<div>Date: {{event.date}} </div>\n<div>Time: {{event.time}}</div>\n<div>Price: ${{event.price}}</div>\n<div> \n<span>Location: {{event.location.address}}</span>\n<span class=\"pad-left\">{{event.location.city}}, {{event.location.country}}</span>\n</div>\n\n</div>\n",
-            styles: ["\n        .pad-left {margin-left: 10px;}\n.well div {color: #bbb;}\n"
+            template: "\n<div class=\"well hoverwell thumbnail\">\n<h2>{{event.name}}</h2>\n<div>Date: {{event.date}} </div>\n<div [ngClass]=\"getStartTimeClass()\" [ngSwitch] = \"event?.time\">Time: {{event.time}}\n<span *ngSwitchCase=\"'8:00 am'\">(Early Start)</span>\n<span *ngSwitchCase=\"'10:00 am'\">(Late Start) </span>\n<span *ngSwitchDefault>(Normal Start)</span>\n</div>\n<div>Price: ${{event.price}}</div>\n<div *ngIf=\"event?.location\"> \n<span>Location: {{event.location?.address}}</span>\n<span class=\"pad-left\">{{event.location?.city}}, {{event.location?.country}}</span>\n</div>\n<div *ngIf=\"event?.onlineUrl\"> \nOnline Url: {{event?.onlineUrl}}\n</div>\n</div>\n",
+            styles: ["\n.green {color: #003300 !important}\n.bold {font-weight: bold}\n.thumbnail {min-height: 210px;}\n.pad-left {margin-left: 10px;}\n.well div {color: #bbb;}\n"
             ]
         }), 
         __metadata('design:paramtypes', [])
