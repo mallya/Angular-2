dcd26e9fd5f42f8d49081b38e575e9d7209df271
Sunil Mallya <ssmallya@gmail.com>
2017-04-05 23:59:12 -0500

    udates

--- app/events/create-event.component.ts
+++ app/events/create-event.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core'
 import {Router} from '@angular/router'
-
+import {EventService} from './shared/event.service'
 @Component({
     templateUrl: 'app/events/create-event.component.html',
     styles: [`
@@ -14,12 +14,14 @@ import {Router} from '@angular/router'
 })
  export class CreateEventComponent {
      isDirty:boolean = true
-     constructor(private router: Router) {
+     constructor(private router: Router, private eventService: EventService) {
          
      }
 
      saveEvent(formValues) {
-         console.log(formValues)
+         this.eventService.saveEvent(formValues)
+         this.isDirty = false
+         this.router.navigate(['events'])
      }
      cancel()
      {
